d1a206cbb529b45dfabe88120c2343f6b27f7281
krasnowsky <wojtek.ref2@gmail.com>
2022-11-07 21:51:59 +0100

    Added request validation

--- WebBackend/app.py
+++ WebBackend/app.py
@@ -1,43 +1,63 @@
+import json
+import requests
+
+from cerberus import Validator
+import configparser
 from flask import Flask, request
 import joblib
-import requests
-import configparser
 
 config = configparser.RawConfigParser()
 config.read('settings.cfg')
-details_dict = dict(config.items('sprinters-config'))
-API_KEY = details_dict['API_KEY']
+settings = dict(config.items('sprinters-config'))
+
+API_KEY = settings['api_key']
+fields = ['offer_type', 'floor', 'area', 'rooms', 'offer_type_of_building', 'market', 'longitude', 'latitude']
+model = joblib.load(settings['model_path'])
+#encoder = joblib.load(settings['encoder_path'])
+
+schema = {
+    'offer_type': {'type': 'string', 'required': True},
+    'floor': {'type': 'float', 'min': -1.00, 'required': True},
+    'area': {'type': 'float', 'min': 10.00, 'required': True},
+    'rooms': {'type': 'integer', 'min': 1, 'required': True},
+    'offer_type_of_building': {'type': 'string', 'required': True},
+    'market': {'type': 'string', 'required': True},
+    'longitude': {'type': 'float', 'required': True},
+    'latitude': {'type': 'float', 'required': True}
+}
+validator = Validator(schema)
 
 app = Flask(__name__)
-fields = ['offer_type', 'floor', 'area', 'rooms', 'offer_type_of_building', 'market', 'longitude', 'latitude']
-model = joblib.load('model.pkl')
 
 def _get_location_data(latitude, longitude):
     url = 'https://geocodeapi.p.rapidapi.com/GetNearestCities'
     location_fields = ['Population', 'CountryId']
-    querystring = {f'latitude':{latitude}, 'longitude':{longitude}, 'range':'0'}
+    querystring = {f'latitude':{str(latitude)}, 'longitude':{str(longitude)}, 'range':'0'}
     headers = {
         'X-RapidAPI-Key': API_KEY,
         'X-RapidAPI-Host': 'geocodeapi.p.rapidapi.com'
     }
     response = requests.request('GET', url, headers=headers, params=querystring)
-    nearest_city = response[0]
+    nearest_city = json.loads(response.content)[0]
     cleared_location = {location_field: nearest_city[location_field] for location_field in location_fields}
     return cleared_location
 
 @app.route('/predict-price', methods=['POST'])
 def predict_price():
     if request.method == 'POST':
+        data = json.loads(request.data)
+        if not validator.validate(data):
+            return validator.errors
         features = {}
         for field in fields:
-            features[field] = request.form.get(field)
+            features[field] = data.get(field)
         location_features = _get_location_data(
             features['latitude'],
             features['longitude']
         )
         if location_features['CountryId'] != 'PL':
             return {'error': 'Please drop pin in boarders of Poland'}, 400
-        features = features | location_features
+        features['population'] = location_features['Population']
         ready_data = encoder.transform(features)
         prediction = model.predict(ready_data)
     return {
