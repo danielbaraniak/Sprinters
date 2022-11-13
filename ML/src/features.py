32628cd012d79bd1fcba56fb446c39bb8563043a
230443 <230443@edu.p.lodz.pl>
2022-11-13 16:37:21 +0100

    Improve transformer

--- ML/src/features.py
+++ ML/src/features.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import OrdinalEncoder, OneHotEncoder
-from sklearn.compose import make_column_transformer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import (
+    StandardScaler,
+    OneHotEncoder,
+    OrdinalEncoder,
+    FunctionTransformer,
+)
 
 columns_order = [
     "offer_type",
@@ -18,58 +24,116 @@ columns_order = [
     "voivodeship",
 ]
 
+dummies_features = [
+    "rooms",
+    "floor",
+]
+
+numeric_features = ["longitude", "latitude", "area", "population"]
+
+onehot_features = []
+
 ordinal_features = [
+    "offer_type",
+    "offer_type_of_building",
+    "market",
     "city_name",
     "voivodeship",
 ]
 
-onehot_features = [
-    "offer_type",
-    "offer_type_of_building",
-    "market",
-]
+
+def dummies(df_e):
+    df_e = pd.get_dummies(df_e)
+    df_e.fillna(value=-2.0, inplace=True)
+    return df_e
 
 
-class Encoder:
-    def __init__(self) -> None:
-        self.transformer = make_column_transformer(
-            (OrdinalEncoder(dtype=np.int32), ordinal_features),
-            (OneHotEncoder(dtype=np.int8), onehot_features),
-            (
-                SimpleImputer(
-                    strategy="constant",
-                    fill_value=-2.0,
-                ),
-                ["floor"],
+def dummies_feature_names_out(_, input_features: list[str]) -> list:
+    result = []
+    for f in input_features:
+        result.append(f)
+        result.append(f + "_na")
+
+    return result
+
+
+numeric_transformer = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        # ("scaler", StandardScaler()),
+    ]
+)
+
+onehot_transformer = Pipeline(
+    steps=[
+        (
+            "imputer",
+            SimpleImputer(
+                strategy="constant",
+                fill_value="missing",
             ),
-            remainder="passthrough",
-        )
+        ),
+        (
+            "onehot",
+            OneHotEncoder(
+                handle_unknown="infrequent_if_exist",
+                max_categories=10,
+            ),
+        ),
+    ]
+)
 
-    def fit(self, df: pd.DataFrame):
-        self.transformer.fit(df)
+ordinal_transformer = Pipeline(
+    steps=[
+        (
+            "imputer",
+            SimpleImputer(
+                strategy="constant",
+                fill_value="missing",
+            ),
+        ),
+        (
+            "ordinal",
+            OrdinalEncoder(encoded_missing_value=-2.0),
+        ),
+    ]
+)
 
-    def transform(self, df: pd.DataFrame, out_path: str = None):
-        df.reindex(columns=columns_order)
-        df = self.transformer.transform(df)
-        if out_path:
-            df.reset_index().to_feather(out_path)
+dummies_transformer = FunctionTransformer(
+    dummies, feature_names_out=dummies_feature_names_out
+)
 
-        return df
+preprocessor = ColumnTransformer(
+    transformers=[
+        ("num", numeric_transformer, numeric_features),
+        ("one_hot", onehot_transformer, onehot_features),
+        # ("dummies", dummies_transformer, dummies_features),
+        ("ordinal", ordinal_transformer, ordinal_features),
+    ],
+    n_jobs=-1,
+    remainder="passthrough",
+)
 
-    def featurize(self, data: dict) -> np.ndarray:
-        d = {k: [v] for k, v in data.items()}
-        df = pd.DataFrame(d)
 
-        return self.transform(df)
-
-
-def get_encoder(dataset_path: str) -> Encoder:
+def get_preprocessor(
+    *, dataset_path: str = None, df: pd.DataFrame = None
+) -> ColumnTransformer:
     """
     dataset_path: path to the file with serialized dataset (ML/data/interim/...)
+    df: dataframe to be used for fitting.
     """
-    df = pd.read_feather(dataset_path)
-    df.drop(["index"], axis=1, inplace=True)
-    print(df.head())
-    enc = Encoder()
-    enc.fit(df)
-    return enc
+    if dataset_path:
+        df = pd.read_feather(dataset_path)
+        df.drop(["index"], axis=1, inplace=True)
+        print(df.head())
+
+    if df is None:
+        raise ValueError("Empty dataframe. Specify 'dataset_path' or 'df'.")
+
+    preprocessor.fit(df)
+    return preprocessor
+
+
+def featurize(preprocessor: ColumnTransformer, data: dict) -> np.ndarray:
+    X = pd.DataFrame({k: [v] for k, v in data.items()})
+    return preprocessor.transform(X)
--- ML/src/main.py
+++ ML/src/main.py
@@ -1,12 +1,27 @@
 from data import clean_dataset
+from features import get_preprocessor
 from features import Encoder, get_encoder
+from sklearn.model_selection import train_test_split
 
 RAW_DATA_PATH = "ML/data/raw/olx_house_price_Q122.csv"
 INTERIM_DATA_PATH = "ML/data/interim/apartments.feather"
 
+target = "price"
+
 
 def main():
     df = clean_dataset(in_path=RAW_DATA_PATH, out_path=INTERIM_DATA_PATH)
+
+    X = df.drop(target, axis=1)
+    y = df[target]
+
+    X, y = preprocessor.fit_transform(X, y)
+
+    X_train, X_valid, y_train, y_valid = train_test_split(
+        X, y, test_size=0.20, random_state=42
+    )
+
+    preprocessor = get_preprocessor(df=df)
 
 
 if __name__ == "__main__":
