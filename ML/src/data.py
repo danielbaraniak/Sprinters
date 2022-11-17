1032f1087cae26d4f5846a7f3b39e125f831dc00
230443 <230443@edu.p.lodz.pl>
2022-11-17 03:58:26 +0100

    Delete redundant things.

--- ML/src/data.py
+++ ML/src/data.py
@@ -3,13 +3,10 @@ import pandas as pd
 from . import features_info
 
 
-def clean_dataset(in_path: str, out_path: str = None) -> pd.DataFrame:
+def clean_dataset(in_path: str) -> pd.DataFrame:
     df_raw = pd.read_csv(in_path, dtype=features_info.types)
     df = df_raw[features_info.useful_columns]
     df = df.dropna(subset=["area", "offer_type_of_building"])
     df = df.where(df["rooms"] <= 4, 4)
-
-    if out_path:
-        df.reset_index().to_feather(out_path)
 
     return df
